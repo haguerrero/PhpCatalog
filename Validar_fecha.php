068f2426b469a42df22b5613a8326b4fd524e38f
Hugo Guerrero <hugoguerrero03@gmail.com>
2019-08-21 10:45:55 -0500

    Update Validar_fecha.php
    Comentarios del script para validad un campo en modo genérico y en un caso de uso.

--- Validar_fecha.php
+++ Validar_fecha.php
@@ -1,30 +1,23 @@
-function validar_fecha($fecha){
-	$valores = explode('/', $fecha);
-	if(count($valores) == 3 && checkdate($valores[1], $valores[0], $valores[2])){
-		return true;
-    }
-	return false;
-}
-
-
-
-if (explode('/', "eatby")) {
-	 setEventMessages($langs->trans("ErrorFieldRequired",
-	  $langs->transnoentitiesnoconv("Fecha de Caducidad")), null, 'errors');
-	 $error++;
-	 $action='correction';
+/************************************** funcion Finalizada **************************************/
+	
+	
+	/* Validacion del Campo de fecha de caducidad. Hugo Guerrero. */
+	$datecadu = GETPOST("eatby");
+	
+	if ($datecadu == 'null') {
+	 $algo = 1;
 	}
 	
-************************************** Armado de la funcion **************************************
-
-function validar_fecha($fecha){
-	$valores = explode('/', $fecha);
-	if(count($valores) == 3 && checkdate($valores[1], $valores[0], $valores[2])){
-		return true;
-    }
-	return false;
+	$valores = explode('/', $datecadu); // Captura de campo para realizar la validacion
 	
-	************************************** funcion Finalizada **************************************
+	if (count($valores) != 3 || !checkdate($valores[1], $valores[0], ($valores[2] != 'null')) || $valores[2]<2019 ) {
+	 $algo = 2;
+	}
+	
+	/*Fin de validacion del campo de fecha de caducidad.*/
+		
+		
+/************************************** funcion Finalizada **************************************/
 	
 	
 	/* Validacion del Campo de fecha de caducidad. Hugo Guerrero. */
